11437b5c80ee2b800e57f8bb5c3dab3651eab9dd
sellersd <david.m.sellers@gmail.com>
2022-01-27 03:22:34 +0000

    attempt to add cstring to array

--- Parser/parser.cpp
+++ Parser/parser.cpp
@@ -34,27 +34,32 @@ char ** parseCommandLine(string CommandLine) {
 
     int MAX_TOKENS = 10;
     int count = 0;
+    char delimiters[] = {'<', '>', '|', ' '};
     char ** ptr_arr[MAX_TOKENS];
 
     // inspiration from
     // https://www.tutorialspoint.com/cpp_standard_library/cpp_string_c_str.htm
     // fixes error converting from const char * to char *
+
     char * c = new char[CommandLine.length()+1];
     strcpy(c, CommandLine.c_str());
 
+    // https://www.geeksforgeeks.org/double-pointer-pointer-pointer-c/
     char * ptr = strtok(c, " ><|") ;
+    // cout << "ptr:\t" << ptr << endl;
     while(ptr != NULL && count < MAX_TOKENS) {
       ptr_arr[count] = &ptr;
-      cout << ptr << endl;
-      ptr = strtok(NULL, " ><|");
+      // cout << strtok(NULL, " ><|\0");
+      ptr = strtok(NULL, delimiters);
+      // cout << "ptr:\t" << ptr << endl;
     }
 
-    cout << "contents of ptr_arr" << endl;
-    cout << "0 " << ptr_arr[0]
-        << "1 " << ptr_arr[1]
-        << "2 " << ptr_arr[2] << endl;
+    cout << "contents of ptr_arr: " << endl;
+    for(int i = 0; i < MAX_TOKENS; i++) {
+      cout << i << '\t' << ptr_arr[i] << endl;
+    }
 
-
+    delete c;
 
      //return &ptr_arr;
 }
