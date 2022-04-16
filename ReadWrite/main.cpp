834840823ff77c67675b4ff29e83586fb41fdb7c
sellersd <david.m.sellers@gmail.com>
2022-04-15 21:09:05 -0500

    Added threads.

--- ReadWrite/main.cpp
+++ ReadWrite/main.cpp
@@ -1,28 +1,39 @@
 #include <thread>
 #include <semaphore>
 #include <iostream>
+#include <fstream>
 #include <deque>
 #include <random>
 #include <sstream>
 #include <chrono>
-
+#include <string>
+#include <typeinfo>
 
 using namespace std;
 
 // Global variables
 deque<string> inputQueue;
 const int SAMPLE_ARRIVAL_RATE = 3;
+  string files[10] = {"sensor0.txt",
+                      "sensor1.txt",
+                      "sensor2.txt",
+                      "sensor3.txt",
+                      "sensor4.txt",
+                      "sensor5.txt",
+                      "sensor6.txt",
+                      "sensor7.txt",
+                      "sensor8.txt",
+                      "sensor9.txt"};
 
 // Instructor provided code to simulate input
 void simulateInput(int rate)
 {
   random_device rd{};
   mt19937 gen{rd()};
-  int count = 0;
 
   poisson_distribution<> pD(rate);
-  normal_distribution<> nD(5.0, 3.0);
-  uniform_int_distribution<int> uD(0,10);
+  normal_distribution<> nD(75.0, 15.0);
+  uniform_int_distribution<int> uD(0,9);
 
   while(true)
   {
@@ -34,9 +45,8 @@ void simulateInput(int rate)
         stringstream sampleStream;
         int deviceNumber = uD(gen);
         float sample = nD(gen);
-        count++;
-        sampleStream << count << " " << deviceNumber << " " << sample;
-        inputQueue.push_front(sampleStream.str());
+        sampleStream << deviceNumber << " " << sample;
+        inputQueue.push_back(sampleStream.str());
       }
     }
     this_thread::sleep_for(chrono::milliseconds(1000));
@@ -49,28 +59,68 @@ void write() {
 }
 
 // Function for threads to read from deque
-void read() {
+void read(int n) {
+  while(true) {
+    cout << endl;
+    cout << "in read" << endl;
+    // lock inputQueue
+    if(!inputQueue.empty()) {
+      cout << "Front is: " << inputQueue.front() << endl;
+      cout << "Front[0] is: " << inputQueue.front()[0] << endl;
+      cout << "Front substr is: " << inputQueue.front().substr(1) << endl;
+      cout << "n: " << n << endl;
+      cout << "Front[0] == n: " << (((int)inputQueue.front()[0] - 48) == n) << endl;
+      bool match  = (((int)inputQueue.front()[0] - 48) == n);
+      if(match) {
+        cout << "Found a reading!" << endl;
 
+
+        ofstream ofile;
+        ofile.open(files[n]);
+        cout << "Writing to file " << n << endl;
+        ofile << inputQueue.front().substr(1);
+        ofile.close();
+        inputQueue.pop_front();
+      }
+    this_thread::sleep_for(chrono::milliseconds(1000));
+    }
+    // unlock inputQueue
+  }
 }
 
 int main(int argc, char *argv[]) {
 
+  thread threads[10];
+  for(int i = 0; i < 10; i++){
+    ofstream ofile;
+    ofile.open(files[i]);
+    ofile.close();
+  }
   inputQueue.push_front("0 START");
   // Build and lanuch the input simulator thread
   thread inputThread(simulateInput, SAMPLE_ARRIVAL_RATE);
   inputThread.detach();
   // Pause three seconds
+  for(int i = 0; i < 10; i++) {
+
+    threads[i] = thread(read, i);
+    threads[i].join();
+  }
+
   cout << "Pausing three seconds for station identification" << endl;
   this_thread::sleep_for(chrono::milliseconds(3000));
   // Now start printing stuff from the queue
   while(true)
   {
-    if (! inputQueue.empty())
-    {
-      string sample = inputQueue.back();
-      inputQueue.pop_back();
-      cout << sample << endl;
-    }
+    cout << "Items in queue:\t" << inputQueue.size() << endl;
+    this_thread::sleep_for(chrono::milliseconds(1000));
+
+    // if (! inputQueue.empty())
+    // {
+    //   string sample = inputQueue.back();
+    //   inputQueue.pop_back();
+    //   cout << sample << endl;
+    // }
   }
 
   return 0;
---
+++ ReadWrite/test.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main(int argc, char *argv[]) {
+
+  char a = '7';
+  int i = (int)a;
+
+  cout << a << '\t' << i << endl;
+
+  return 0;
+}
